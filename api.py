a21d0022ec96c2d436b53df0b34b2573e757f870
Felipe Meneguzzi <felipe.meneguzzi@universo.univates.br>
2025-04-07 14:28:22 +0000

    Fix import statements and correct comments in api.py

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from routers import users, login
-from handlers.exceptions import (
+from routers import Users, Login
+from handlers.Exceptions import (
     custom_route_not_found_exception_handler,
     custom_internal_server_error_handler,
 )
 
 app = FastAPI()
 
-# Registrar as rotas
-app.include_router(users.router)
-app.include_router(login.router)
+# Registrar as rotass
+app.include_router(Users.router)
+app.include_router(Login.router)
 
 # Registrar os handlers personalizados
 app.add_exception_handler(StarletteHTTPException,
