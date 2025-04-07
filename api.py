01757561c226aa4c091e4fe3e82f3aabca0294d8
Felipe Meneguzzi <felipe.meneguzzi@universo.univates.br>
2025-04-07 18:28:11 +0000

    Refactor AuthMiddleware implementation and update API structure to use middleware correctly

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware import Middleware
+from middlewares import AuthMiddleware
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from routers import LoginRouter, UserRouter
 from handlers.ExceptionsHandler import (
@@ -8,15 +9,16 @@ from handlers.ExceptionsHandler import (
 )
 from middlewares.AuthMiddleware import AuthMiddleware
 
+auth_app = FastAPI(middleware=[Middleware(AuthMiddleware)])
+auth_app.include_router(router=UserRouter.router, prefix="/users")
+
 app = FastAPI()
 
 # Registrar as rotass
-app.include_router(router=UserRouter.router, prefix="/users",
-                   dependencies=[Depends(AuthMiddleware)])
+app.mount("/auth", auth_app)
 
 app.include_router(router=LoginRouter.router, prefix="/login")
 
-# Registrar os handlers personalizados
 app.add_exception_handler(StarletteHTTPException,
                           custom_route_not_found_exception_handler)
 app.add_exception_handler(Exception, custom_internal_server_error_handler)
--- middlewares/AuthMiddleware.py
+++ middlewares/AuthMiddleware.py
@@ -1,16 +1,20 @@
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
 from services.BaseService import BaseService, HTTPStatus
 from valueObjects.AuthTokenObject import decode_token
 
 
-async def AuthMiddleware(request: Request):
-    if not request.headers.get("Authorization"):
-        return BaseService.send_response(data="Token nao informado", status_code=HTTPStatus.BAD_REQUEST)
+class AuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if not request.headers.get("Authorization"):
+            return BaseService.send_response(data="Token nao informado", status_code=HTTPStatus.BAD_REQUEST)
 
-    token = request.headers.get("Authorization")
-    response = decode_token(token)
+        token = request.headers.get("Authorization")
+        response = decode_token(token)
 
-    if response == "E":
-        return BaseService.send_response(data="Token expirado", status_code=HTTPStatus.UNAUTHORIZED)
-    if response == "I":
-        return BaseService.send_response(data="Token invalido", status_code=HTTPStatus.UNAUTHORIZED)
+        if response == "E":
+            return BaseService.send_response(data="Token expirado", status_code=HTTPStatus.UNAUTHORIZED)
+        if response == "I":
+            return BaseService.send_response(data="Token invalido", status_code=HTTPStatus.UNAUTHORIZED)
+
+        return await call_next(request)
