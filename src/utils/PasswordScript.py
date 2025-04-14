71310e55bf6b2ac0f61fd45fc70f2bcee58212d3
Felipe Meneguzzi <felipe.meneguzzi@universo.univates.br>
2025-04-14 15:21:59 -0300

    Refactor password utility functions: ensure hashed password is returned as a string and improve variable naming for clarity

--- src/utils/PasswordScript.py
+++ src/utils/PasswordScript.py
@@ -5,9 +5,10 @@ def generate_hash(password):
     pwd_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
-    return hashed_password
+    return hashed_password.decode('utf-8')
 
 
 def verify_hash(plain_password, hashed_password):
-    password_byte_enc = plain_password.encode('utf-8')
-    return bcrypt.checkpw(password=password_byte_enc, hashed_password=hashed_password)
+    plain_password_byte_enc = plain_password.encode('utf-8')
+    hashed_password_byte_enc = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password=plain_password_byte_enc, hashed_password=hashed_password_byte_enc)
